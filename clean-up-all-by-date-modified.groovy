a44817bbf02a8bb8bf50cd47b9dcaab68c44a455
samboleap3008 <samboleap3008@gmail.com>
2023-09-22 10:59:39 +0700

    fix script

--- clean-up-all-by-date-modified.groovy
+++ clean-up-all-by-date-modified.groovy
@@ -3,59 +3,49 @@ import jenkins.model.*
 import hudson.*
 import hudson.model.*
 
-//manager.listener.logger.println new Date(System.currentTimeMillis()).format('MM/dd/yyyy hh:mm:ss a') + " / " + " -- Start Time" 
+// manager.listener.logger.println new Date(System.currentTimeMillis()).format('MM/dd/yyyy hh:mm:ss a') + " / " + " -- Start Time"
 
-//Get value from String Parameter
+// Get value from String Parameter
 MAX_BUILDS = manager.build.buildVariables.get("MAX_BUILDS").toInteger()
 
-for (job in Jenkins.instance.items) 
-{
-  
-  	int count = 0
-  	
-    manager.listener.logger.println "\n ***Job Name: "+job.name+"***"
-    
-        if(job.workspace!=null && job.workspace!="")  //Check if there is a workspace associated with the Job
-        {
-            manager.listener.logger.println "Workspace path : " + job.workspace
-            
-            String workspace = job.workspace
-            
-            File folder = new File(workspace)
-            
-            if(folder!=null && folder.exists()) //Check if the Workspace folder exists
-            {
-                // Get all files and folders within the Workspace of current job. 
-                //Iterate through only folders and sort em by Modified Date.
-                
-                File[] files = new File(workspace).listFiles().sort(){
-                a,b -> b.lastModified().compareTo a.lastModified()
-                }
-                .each{
-                    if(!it.isFile()) //Check only for folders
-                    {
-                        if(count < MAX_BUILDS)
-                            manager.listener.logger.println new Date(it.lastModified()).format('MM/dd/yyyy hh:mm:ss a') + " /" + it.name + " -- Save" 
-                        else
-                        {
-                            manager.listener.logger.println new Date(it.lastModified()).format('MM/dd/yyyy hh:mm:ss a') + " /" + it.name + " ** Deleted" 
-                            it.deleteDir()
-                        }
-                        count++
-                    }
-                }
+Jenkins.instance.getAllItems().each { job ->
+  int count = 0
+  manager.listener.logger.println("\n***Job Name: " + job.name + "***")
+
+  if (job instanceof AbstractProject) {
+    AbstractProject project = (AbstractProject) job
+    FilePath workspacePath = project.getSomeWorkspace()
+
+    if (workspacePath != null) { // Check if there is a workspace associated with the job
+      String workspace = workspacePath.getRemote()
+      manager.listener.logger.println("Workspace path: " + workspace)
+
+      File folder = new File(workspace)
+
+      if (folder != null && folder.exists()) { // Check if the Workspace folder exists
+        // Get all files and folders within the Workspace of the current job.
+        // Iterate through only folders and sort them by Modified Date.
+
+        File[] files = folder.listFiles().sort { a, b ->
+          b.lastModified().compareTo(a.lastModified())
+        }.each { file ->
+          if (!file.isFile()) { // Check only for folders
+            if (count < MAX_BUILDS) {
+              manager.listener.logger.println(new Date(file.lastModified()).format('MM/dd/yyyy hh:mm:ss a') + " / " + file.name + " -- Save")
+            } else {
+              manager.listener.logger.println(new Date(file.lastModified()).format('MM/dd/yyyy hh:mm:ss a') + " / " + file.name + " ** Deleted")
+              file.deleteDir()
             }
-            else
-            {
-                manager.listener.logger.println "Workspace is empty or doesn't exist"
-            }
+            count++
+          }
         }
-        else
-        {
-            manager.listener.logger.println "No Workspace associated with this job"
-        }
+      } else {
+        manager.listener.logger.println("Workspace is empty or doesn't exist")
+      }
+    } else {
+      manager.listener.logger.println("No Workspace associated with this job")
     }
-
+  } else {
+    manager.listener.logger.println("Skipping unsupported job type: " + job.name)
+  }
 }
-
-//manager.listener.logger.println new Date(System.currentTimeMillis()).format('MM/dd/yyyy hh:mm:ss a') + " / " + " -- End Time" 
